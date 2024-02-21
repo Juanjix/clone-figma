b264568f7868c9b2e53c0ebcba603a1a01888c95
Juan José Peralta <juanjose@indicius.com>
2024-02-21 15:41:58 -0300

    I added colors foundations

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      white: "#ff",
+      gray: "#9CA6AB",
+      black: "#27",
+      red: "#7B9AA8",
+      blue: "#128DD3",
+      yellow: "#FFE700",
+      teal10: "#F0F3F4",
+      teal20: "#C0CDD3",
+      teal30: "#7B9AA8",
+    },
+  },
+  plugins: [],
+};
