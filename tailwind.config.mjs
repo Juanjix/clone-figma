5b3a1400be667ad108e727217a12646e101a05db
Juan José Peralta <juanjose@indicius.com>
2024-02-22 10:01:31 -0300

    foundations / I added screens and spacings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,12 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     colors: {
       white: "#ff",
       gray: "#9CA6AB",
@@ -14,6 +20,15 @@ export default {
       teal20: "#C0CDD3",
       teal30: "#7B9AA8",
     },
+    spacing: {
+      1: "8px",
+      2: "12px",
+      3: "16px",
+      4: "24px",
+      5: "32px",
+      6: "48px",
+    },
+    fontFamily: {},
   },
   plugins: [],
 };
